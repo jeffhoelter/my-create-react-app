c943f2b9c9ef71a9f9335fb48d2c14d101188657
jeffhoelter <jeffh@loar.net>
2017-04-28 11:21:43 -0400

    Initial commit of my create-react-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const App = function () {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        Hey now To 123 234 get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+
+export default App;
